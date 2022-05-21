a98f5e3ff89ba947504c4d884e0cda390f36cb75
nguyenhuuluan94 <nguyenhuuluan94@gmail.com>
2022-05-21 16:00:23 +0700

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
 const productRoute = require("./routes/product.route");
 const cartRoute = require("./routes/cart.route");
 const authRoute = require("./routes/auth.route");
@@ -10,6 +11,7 @@ mongoose
     .then(() => console.log("Connect to Mongo"))
     .catch((err) => console.log("Could not connect to Mongo", err));
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoute);
