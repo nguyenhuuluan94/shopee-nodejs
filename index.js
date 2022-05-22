1f95886f91287cbd9133e03b960f6a9cc88e6b25
nguyenhuuluan94 <nguyenhuuluan94@gmail.com>
2022-05-22 15:57:45 +0700

    add cors

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ mongoose
     .then(() => console.log("Connect to Mongo"))
     .catch((err) => console.log("Could not connect to Mongo", err));
 const app = express();
-app.use(cors());
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 app.use(express.json());
 
 app.use("/api/products", productRoute);
